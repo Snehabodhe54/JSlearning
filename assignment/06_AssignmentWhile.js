f0d7004e0e029d66e730a473f5efd5fd473ba879
Snehabodhe54 <139098425+Snehabodhe54@users.noreply.github.com>
2023-07-31 11:57:17 +0530

    Assignment using for loop and while loop.
    Assignment using for loop and while loop

---
+++ assignment/06_AssignmentWhile.js
@@ -0,0 +1,51 @@
+var index = 5;
+ while(index <=15) {
+    console.log( index);
+    index++;
+    }
+    
+    console.log(`------------------------------------------------`);
+var index = 50;
+ while(index >=40) {
+    console.log( index);
+    index--;
+    }
+    
+    console.log(`------------------------------------------------`);
+var index = 1;
+ while(index <=30) {
+    console.log( index);
+    index=index+2;
+    }
+    
+    console.log(`------------------------------------------------`);
+var index = 0;
+ while(index <=20) {
+    console.log( index);
+    index=index+2;
+    }
+    
+    console.log(`------------------------------------------------`);
+var index = 5;
+ while(index <=50) {
+    console.log( index);
+    index=index+5;
+    }
+    
+    console.log(`------------------------------------------------`);
+var index = 10;
+ while(index <=100) {
+    console.log( index);
+    index=index+10;
+    }
+
+    console.log(`------------------------------------------------`);
+var index = 100;
+ while(index >=10) {
+    console.log( index);
+    index=index-10;
+    }
+    
+
+    
+    
