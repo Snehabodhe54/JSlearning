c29ddbeebdc1cbca8bfef9c6d16f3e26b1390ede
Snehabodhe54 <139098425+Snehabodhe54@users.noreply.github.com>
2023-08-03 12:22:04 +0530

    today we learn array, map & object.
    today we learn array, map & object.

---
+++ 09_array1.js
@@ -0,0 +1,19 @@
+console.log(`------ concat ----------`);
+let array1 = [3, 4, 5, 6];
+let array2 = [6, 7, 8, 9];
+let array3 = array1.concat(array2);
+console.log(array3);
+
+console.log(`------ join ----------`);
+let result = array1.join(",");
+console.log(result);
+
+console.log(`------ for of loop ----------`);
+for (const element of array3) {
+    console.log(element);
+}
+
+console.log(`------ array resize ----------`);
+let array4 = [4, 5, 6, 7, 8, 9];
+array4.length = 3;
+console.log(array4);
