985ab11ba0c3febac72fff44a3dd12b2f0a11228
Snehabodhe54 <snehabodhe22@gmail.com>
2023-08-21 11:08:00 +0530

    Assignment of rest parameter
    Assignment of rest parameter

--- 16_restParamater.js
+++ 16_restParamater.js
@@ -1,6 +1,12 @@
 function add(num1, num2, ...num ){
     // console.log(`num1 ${num1},  num2 ${num2}`);
     // console.log(num);
+    let addition = num1+ num2;
+   for (const value of num) {
+    addition= addition+value;
+   
+   }
+   console.log(`Addition of ${num1}, ${num2 } and rest of numbers (i.e ${num}) is : ${addition}`);
 }
 add(5, 6);
 add(9, 5, 6);
